16bbc70328e1ba3cd97be35a621bb67c5a33d839
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-23 16:08:14 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,12 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = Float::INFINITY
+  lowest_value = nil
   name_hash.each do |k, v|
-    if v < lowest_value
+    if lowest_value == nil
+      lowest_value = v
+      lowest_key = k
+    elsif v < lowest_value
       lowest_value = v
       lowest_key = k
     end
